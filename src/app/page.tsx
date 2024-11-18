4ebdd36ae7ec761a1e75088b6547857fdddeefdc
xenohunter <xenohunter@pm.me>
2024-11-19 00:17:34 +0100

    Update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home(): JSX.Element {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image src="/logo-transparent.png" alt="#define GAMES logo" fill={true} priority />
+        <Image src="/logo-transparent-bold.png" alt="#define GAMES logo" fill={true} priority />
       </main>
       <footer className={styles.footer}>
         <a href="https://x.com/definegames" target="_blank" rel="noopener noreferrer">
